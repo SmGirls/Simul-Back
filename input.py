301f62d985cc5d2d3b133d3a5d92ee6c670bf603
juya <kjy2967@naver.com>
2023-05-12 22:24:00 +0900

    테스트 데이터 생성

---
+++ input.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+
+START_ROW = int(os.getenv('START_ROW' , 1)) # 환경 변수로 시작 row 받아오기
+
+# csv skiprows ~ 10개 읽어오기
+# csv type : DataFrame
+csv = pd.read_csv("test_input.csv" , encoding='utf-8-sig'
+                ,header=None,skiprows=START_ROW,nrows = 10)
+
+items_data = csv.values.tolist() # DataFrame -> list 변환
