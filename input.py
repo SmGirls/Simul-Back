5a010f2d4f24d1119f0e3eeeae1f8752b9e8c01b
ellin0817 <ellin0817@naver.com>
2023-05-13 00:55:59 +0900

    일부 수정

--- input.py
+++ input.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import os
 
-
-#TART_ROW = int(os.getenv('START_ROW' , 1)) # 환경 변수로 시작 row 받아오기
+START_ROW = int(os.getenv('START_ROW' , 1)) # 환경 변수로 시작 row 받아오기
 
 print(os.getenv('START_ROW'))
 
 # csv skiprows ~ 10개 읽어오기
 # csv type : DataFrame
 csv = pd.read_csv("test_input.csv" , encoding='utf-8-sig'
-                ,header=None,skiprows=int(os.getenv('START_ROW')),nrows = 10)
+                ,header=None,skiprows=os.getenv('START_ROW'),nrows = 10)
 
 items_data = csv.values.tolist() # DataFrame -> list 변환
