fdf236ab551070173c5b3332119da1c761aac5b1
ellin0817 <ellin0817@naver.com>
2023-05-13 01:50:15 +0900

    시각화 코드 수정

--- visualize.py
+++ visualize.py
@@ -3,7 +3,8 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from input import items_data
+from Data.input import items_data
+import os
 
 # Container 가로, 세로, 높이
 containers = [
@@ -49,12 +50,13 @@ for t in range(len(containers)):
     truckY = containers[t][1]
     truckZ = containers[t][2]
 
-    packer.add_bin(Bin('LB', truckX, truckY, truckZ, 3000.0))
+    #무게 제약 -> 4번째 요소
+    packer.add_bin(Bin('LB', truckX, truckY, truckZ, 10000.0))
 
     # 물류 등록 - (물류 이름, 가로, 세로, 높이, 무게) * 수량
-    for i in range(len(items)):
+    for i in range(1,len(items)):
         # 물류의 수량 만큼 곱해주기기
-        each_items_cnt = items[i][5]
+        each_items_cnt = int(items[i][5])
         # 물류의 수량이 0이면 안됨^^
         if each_items_cnt > 0:
             for _ in range(each_items_cnt):
