0e729770d3fbed6e4fa8c897a2354ff7927fe338
ellin0817 <ellin0817@naver.com>
2023-08-04 07:25:22 +0000

    [Back/FEAT] #5 물류 정보 물류 정보 select

--- app.py
+++ app.py
@@ -67,7 +67,26 @@ def struck_list_delete(productname):
             return jsonify(struck_list)
     return jsonify("Failure! Index Error!")
 
-@app.route('/container_manage')
+# 선택한 물류 보기 
+@app.route('/select_list', methods=['GET'])
+def view_select_list():
+    global select_list
+    return jsonify(select_list)
+
+# 물류 정보 중 컨테이너에 적재하고 싶은 물류 선택
+@app.route('/select_list', methods=['POST'])
+def create_select_list():
+    data = request.get_json()    # post로 이런 json 형식으로 넘겨주면 됨 {"selected_product_list": ["box1", "box2"]}
+    selected_product_names = data['selected_product_list']
+    
+    global select_list
+    # Create the select_list based on the selected product names
+    select_list = [item for item in struck_list if item['productname'] in selected_product_names]
+    
+    return jsonify(select_list)
+
+# 시뮬레이션한 물류에 대한 컨테이너 적재 이미지 리스트
+@app.route('/container_list')
 def container_manage():
     return "container_manage"
 
