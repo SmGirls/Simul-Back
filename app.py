f774acfbdf6897cc4201f4c7e31526325b24056f
ellin0817 <ellin0817@naver.com>
2023-09-04 14:03:47 +0000

    [Back/FEAT] #6 시뮬레이션(물류 적재 시각화) 기능 개발

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 import json
 
-import visualize_test
+import visualize_func
 
 # app = Flask(__name__)
 app = Flask(__name__,
@@ -70,33 +70,37 @@ def struck_list_delete(productname):
             return jsonify(struck_list)
     return jsonify("Failure! Index Error!")
 
-# 선택한 물류 보기 
-@app.route('/select_list', methods=['GET'])
-def view_select_list():
-    global select_list
-    return jsonify(select_list)
+# # 선택한 물류 보기 
+# @app.route('/select_list', methods=['GET'])
+# def view_select_list():
+#     global select_list
+#     return jsonify(select_list)
 
-# 물류 정보 중 컨테이너에 적재하고 싶은 물류 선택
-@app.route('/select_list', methods=['POST'])
-def create_select_list():
+# # 물류 정보 중 컨테이너에 적재하고 싶은 물류 선택
+# @app.route('/select_list', methods=['POST'])
+# def create_select_list():
+#     global select_list
+    
+#     data = request.get_json()    # post로 이런 json 형식으로 넘겨주면 됨 {"selected_product_list": ["box1", "box2"]}
+#     selected_product_names = data['selected_product_list']
+    
+#     # Create the select_list based on the selected product names
+#     select_list = [item for item in struck_list if item['productname'] in selected_product_names]
+#     print("값:",select_list) # [{'productname': 'box1', 'width': 250, 'depth': 220, 'height': 220, 'weight': 100, 'count': 5}, {'productname': 'box2', 'width': 180, 'depth': 160, 'height': 250, 'weight': 300, 'count': 4}]
+#     return jsonify(select_list)
+
+# 물류 선택 후 시뮬레이션
+@app.route('/simulation', methods=['POST'])
+def post_simulation():
     global select_list
+    select_items_list = []
     
     data = request.get_json()    # post로 이런 json 형식으로 넘겨주면 됨 {"selected_product_list": ["box1", "box2"]}
     selected_product_names = data['selected_product_list']
     
     # Create the select_list based on the selected product names
     select_list = [item for item in struck_list if item['productname'] in selected_product_names]
-    
-    return jsonify(select_list)
 
-# 선택한 물류에 대해 시뮬레이션
-@app.route('/simulation', methods=['GET'])
-def show_simulation():
-    global select_list
-    select_items_list=[]
-    containers = [
-        [1000, 1000, 1000],
-    ]
     for item in select_list:
         select_items_list.append([
             item['productname'],
@@ -105,10 +109,16 @@ def show_simulation():
             item['height'],
             item['weight'],
             item['count']
-        ])   
-    visualize_test.simulation(containers,select_items_list)  # Pass the select_list to visualize.py
-    #return render_template('simulation.html', select_list=select_list)
-    return True
+        ]) 
+    # 컨테이너 사이즈 
+    containers = [
+        [1000, 1000, 1000],
+    ]
+    # 이미지 이름 저장
+    img_name = item['productname']
+    visualize_func.simulation(containers,select_items_list,img_name)  # Pass the select_list to visualize.py
+    return jsonify(select_items_list)
+
 
 # 시뮬레이션한 물류에 대한 컨테이너 적재 이미지 리스트
 @app.route('/container_list')
