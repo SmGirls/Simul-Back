30be78ba1223220b2a230f995ced07f04ecb7884
juya <kjy2967@naver.com>
2023-09-09 23:44:24 +0900

    [FEAT]: simulation 분리 및 api화

--- visualize_func.py
+++ visualize_func.py
@@ -1,12 +1,59 @@
+import datetime
+
 from py3dbp import Packer, Bin, Item
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg') # 메인스레드에서 matplotlib 실행 안할 경우
 import matplotlib.pyplot as plt
-import random
-import os
 import time
+import random
+import string
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+
+
+
+@app.route('/', methods=['POST'])
+def run_simulation():
+    data = request.get_json()  # 요청 데이터를 JSON 형태로 받음
+    containers = data['containers']
+    items = data['items']
+    # 랜덤한 이미지 이름과 컨테이너 이름 생성
+    img_name = generate_random_string(10)
+    container_name = generate_random_string(10)
+
+    # simulation 함수 실행
+    total_count, unloadable_count, loaded_weight,execution_date = simulation(containers, items, img_name)
+
+    formatted_date = execution_date.strftime('%Y-%m-%d %H:%M:%S')
+    return jsonify({
+        "container_name":container_name,
+        "total_count": total_count,
+        "unloadable_count": unloadable_count,
+        "loaded_weight": loaded_weight,
+        "image": img_name,
+        "execution_date": formatted_date
+    })
+
+def generate_random_string(length):
+    # string.ascii_letters는 모든 대소문자 알파벳을 포함합니다.
+    # string.digits는 0부터 9까지의 숫자를 포함합니다.
+    letters_and_digits = string.ascii_letters + string.digits
+
+    # 지정된 길이만큼의 랜덤한 문자열을 반환합니다.
+    return ''.join(random.choice(letters_and_digits) for _ in range(length))
 def simulation(containers,items,img_name):
+    # 현재 날짜와 시간 가져오기
+    execution_date = datetime.datetime.now()
+    total_count = 0 # 총 물류 수
+    unloadable_count = 0 # 적재 불가 물류 수
+    loaded_weight = 0 # 적재 물류 무게
+
+
+
     colorList = ["crimson", "limegreen", "g", "r", "c", "m", "y", "k"]
     # 3D화
     def cuboid_data2(o, size=(1, 1, 1)):
@@ -48,6 +95,7 @@ def simulation(containers,items,img_name):
         for i in range(len(items)):
             # 물류의 수량 만큼 곱해주기기
             each_items_cnt = int(items[i][5])
+            total_count += each_items_cnt # 총 물류 수 구하기
             # 물류의 수량이 0이면 안됨^^
             if each_items_cnt > 0:
                 for _ in range(each_items_cnt):
@@ -78,12 +126,14 @@ def simulation(containers,items,img_name):
                 # 적합한 물류는 적재하고 item list에서 빼주기
                 # print("적합 물류 정보 : ", item.name)
                 fitted_list.append(item.name)
+                loaded_weight += item.weight # 적재된 물류 무게
             print(fitted_list)
             fitted_rows = [i for i, item in enumerate(items) if item[0] in fitted_list]
 
             print("UNFITTED ITEMS:")
             for item in b.unfitted_items:
                 print("====> ", item.string())
+                unloadable_count += 1 # 적재 불가 물류 수
 
             print("***************************************************")
             print("***************************************************")
@@ -111,7 +161,8 @@ def simulation(containers,items,img_name):
         ax.set_zlim([0, truckZ])
 
         time.sleep(10)
-        #plt.savefig("./share/static/images/result"+ os.getenv('START_ROW', '1') + ".png")
+        plt.savefig("./share/static/images/"+ img_name + ".png")
         #plt.savefig("./result.png")
-        plt.savefig(img_name+".png")
-        # 글자 먼저 받아보고 그다음으로 이미지 받아오는걸로 해보기 
+        #plt.savefig(img_name+".png")
+
+        return total_count,unloadable_count,loaded_weight,execution_date
