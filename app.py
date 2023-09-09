d3c044ef000ee71e36bda6bc3830e392ee8954e9
juya <kjy2967@naver.com>
2023-09-09 23:43:54 +0900

    [FEAT]: simul api http post 요청 보내기 & 100개 slicing

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for, request, jsonify
-from flask_restx import Resource, Namespace, Api
 from flask_cors import CORS
 import pandas as pd
-import os
 import json
 import requests
 
@@ -111,28 +109,164 @@ def post_simulation():
     
     data = request.get_json()    # post로 이런 json 형식으로 넘겨주면 됨 {"selected_product_list": ["box1", "box2","box3."]}
     selected_product_names = data['selected_product_list']
-    
+
     # Create the select_list based on the selected product names
     select_list = [item for item in struck_list if item['productname'] in selected_product_names]
 
-    for item in select_list:
-        select_items_list.append([
-            item['productname'],
-            item['width'],
-            item['depth'],
-            item['height'],
-            item['weight'],
-            item['count']
-        ]) 
-    # 컨테이너 사이즈 
+    # 컨테이너 사이즈
     containers = [
         [1000, 1000, 1000]
     ]
-    # 이미지 이름 저장
-    img_name = item['productname']
-    visualize_func.simulation(containers,select_items_list,img_name)  # Pass the select_list to visualize.py 이부분은ㄹ rest api로 따서 넘겨줘야됨 
-    return jsonify(select_items_list)
+    # 물류 100개 넣었을 경우
+    if len(selected_product_names) == 100:
+        all_select_items_lists = slicing_data(select_list)
+        # Initialize an empty dictionary to store all responses
+        all_responses = {}
 
+        # First request
+        data1 = {
+            'containers': containers,
+            'items': all_select_items_lists[0],
+        }
+        response1 = requests.post("http://127.0.0.1:7001/", json=data1)
+        if response1.status_code == 200:
+            all_responses["response_1"] = response1.json()
+
+        # Second request
+        data2 = {
+            'containers': containers,
+            'items': all_select_items_lists[1],
+        }
+        response2 = requests.post("http://127.0.0.1:7001/", json=data2)
+        if response2.status_code == 200:
+            all_responses["response_2"] = response2.json()
+
+        # 3 request
+        data3 = {
+            'containers': containers,
+            'items': all_select_items_lists[2],
+        }
+        response3 = requests.post("http://127.0.0.1:7001/", json=data3)
+        if response3.status_code == 200:
+            all_responses["response_3"] = response3.json()
+
+        # 4 request
+        data4 = {
+            'containers': containers,
+            'items': all_select_items_lists[3],
+        }
+        response4 = requests.post("http://127.0.0.1:7001/", json=data4)
+        if response4.status_code == 200:
+            all_responses["response_4"] = response4.json()
+
+
+        # 5 request
+        data5 = {
+            'containers': containers,
+            'items': all_select_items_lists[4],
+        }
+        response5 = requests.post("http://127.0.0.1:7001/", json=data5)
+        if response5.status_code == 200:
+            all_responses["response_5"] = response5.json()
+
+        # 6 request
+        data6 = {
+            'containers': containers,
+            'items': all_select_items_lists[5],
+        }
+        response6 = requests.post("http://127.0.0.1:7001/", json=data6)
+        if response6.status_code == 200:
+            all_responses["response_6"] = response6.json()
+
+        # 7 request
+        data7 = {
+            'containers': containers,
+            'items': all_select_items_lists[6],
+        }
+        response7 = requests.post("http://127.0.0.1:7001/", json=data7)
+        if response7.status_code == 200:
+            all_responses["response_7"] = response7.json()
+
+        # 8 request
+        data8 = {
+            'containers': containers,
+            'items': all_select_items_lists[7],
+        }
+        response8 = requests.post("http://127.0.0.1:7001/", json=data8)
+        if response8.status_code == 200:
+            all_responses["response_8"] = response8.json()
+
+        # 9 request
+        data9 = {
+            'containers': containers,
+            'items': all_select_items_lists[8],
+        }
+        response9 = requests.post("http://127.0.0.1:7001/", json=data9)
+        if response9.status_code == 200:
+            all_responses["response_9"] = response9.json()
+
+        # Tenth request
+        data10 = {
+            'containers': containers,
+            'items': all_select_items_lists[9],
+        }
+        response10 = requests.post("http://127.0.0.1:7001/", json=data10)
+        if response10.status_code == 200:
+            all_responses["response_10"] = response10.json()
+
+        # Convert all_responses to a JSON string for storage or display, after you've received and processed every single one of them.
+        all_responses_json = json.dumps(all_responses)
+        return all_responses_json
+
+    else:
+        for item in select_list:
+            select_items_list.append([
+                item['productname'],
+                item['width'],
+                item['depth'],
+                item['height'],
+                item['weight'],
+                item['count']
+            ])
+        # request 통신
+        url = "http://127.0.0.1:7001/"
+        data = {
+            'containers': containers,
+            'items': select_items_list,
+        }
+        response = requests.post(url, json=data)  # POST 요청 보내기
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return "Error: {}".format(response.status_code)
+
+
+
+    #visualize_func.simulation(containers,select_items_list,img_name)  # Pass the select_list to visualize.py 이부분은ㄹ rest api로 따서 넘겨줘야됨
+    #return jsonify(select_items_list)
+
+def slicing_data(select_list):
+    # Initialize a list to hold all select_items_lists
+    all_select_items_lists = []
+
+    # Loop over select_list in chunks of 10
+    for i in range(0, len(select_list), 10):
+        # Create a new select_items_list for every chunk of 10 items
+        select_items_list = []
+        for item in select_list[i:i + 10]:
+            select_items_list.append([
+                item['productname'],
+                item['width'],
+                item['depth'],
+                item['height'],
+                item['weight'],
+                item['count']
+            ])
+        # Add this list to the overall list
+        all_select_items_lists.append(select_items_list)
+
+    return all_select_items_lists
 
 # 시뮬레이션한 물류에 대한 컨테이너 적재 이미지 리스트
 @app.route('/container_list')
